9943c76a38a84ebf9fb49c69b4ca5fa3c7a038e9
dan dao <dandaosocal@gmail.com>
2020-05-23 14:16:37 -0500

    Added notation

--- Updated Ebay Script with CSV .py
+++ Updated Ebay Script with CSV .py
@@ -1,18 +1,19 @@
+#Part 0: Leading the Packages 
+###
+pip ebaysdk
 from ebaysdk.finding import Connection as finding
 from bs4 import BeautifulSoup
 
+#Part 1: Connecting to the eBay Platform. You will need to obtain a ID_APP from ebay developers website. https://developer.ebay.com/
 ID_APP = 'INSERT_YOUR_ID'
-
 api = finding(https=True, appid=ID_APP, config_file=None)
-#response = api.execute('findCompletedItems', { 'keywords': 'Nikon FM' })
 response = api.execute('findCompletedItems', { 'keywords': 'Nikon FM' }, {'categoryId':'15230'})
 
-
+#Part 2: Using Beautiful Soup to parse the content. 
 soup = BeautifulSoup(response.content,'lxml')
-#totalentries = int(soup.find('totalentries').text)
 items = soup.find_all('item')
 
-#Creating the CSV file 
+#Part 3: Writing your pull to a CSV file 
 
 import csv
 table = items
